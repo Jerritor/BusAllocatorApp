379b9394177571e1351d65999c62ac57936e5c7e
Jerritor <jambtor@gmail.com>
2024-09-27 08:59:45 +0800

    Added UI components for settings and renamed them based on functionality

--- SettingsForm.Designer.cs
+++ SettingsForm.Designer.cs
@@ -35,8 +35,27 @@
             deptsModeRadioButton = new RadioButton();
             totalModeRadioButton = new RadioButton();
             closeWindowButton = new Button();
+            incompleteAllocsCheckBox = new CheckBox();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            label2 = new Label();
+            label3 = new Label();
+            this.allocationFolderTextBox = new TextBox();
+            this.allocationFolderUploadButton = new Button();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            allocationFileNameTextBox = new TextBox();
+            label5 = new Label();
+            tableLayoutPanel5 = new TableLayoutPanel();
+            tableLayoutPanel6 = new TableLayoutPanel();
+            bufferSizesButton = new Button();
+            busRateButton = new Button();
+            label4 = new Label();
+            advancedSettingsButton = new Button();
             tableLayoutPanel1.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            tableLayoutPanel5.SuspendLayout();
+            tableLayoutPanel6.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -45,21 +64,30 @@
             tableLayoutPanel1.ColumnCount = 1;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
             tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
-            tableLayoutPanel1.Controls.Add(closeWindowButton, 0, 2);
+            tableLayoutPanel1.Controls.Add(closeWindowButton, 0, 6);
+            tableLayoutPanel1.Controls.Add(incompleteAllocsCheckBox, 0, 1);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel3, 0, 3);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel4, 0, 4);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel5, 0, 5);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel6, 0, 2);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 4;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 150F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowCount = 7;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 140F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 80F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
-            tableLayoutPanel1.Size = new Size(511, 450);
+            tableLayoutPanel1.Size = new Size(511, 471);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
             // 
             tableLayoutPanel2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel2.BackColor = SystemColors.ControlLightLight;
             tableLayoutPanel2.ColumnCount = 2;
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
@@ -71,9 +99,9 @@
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 3;
             tableLayoutPanel2.RowStyles.Add(new RowStyle());
-            tableLayoutPanel2.RowStyles.Add(new RowStyle());
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel2.Size = new Size(505, 144);
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(505, 134);
             tableLayoutPanel2.TabIndex = 0;
             // 
             // label1
@@ -118,7 +146,7 @@
             totalModeRadioButton.AutoSize = true;
             totalModeRadioButton.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             totalModeRadioButton.ForeColor = Color.DimGray;
-            totalModeRadioButton.Location = new Point(3, 98);
+            totalModeRadioButton.Location = new Point(3, 85);
             totalModeRadioButton.Name = "totalModeRadioButton";
             totalModeRadioButton.Size = new Size(176, 25);
             totalModeRadioButton.TabIndex = 3;
@@ -130,7 +158,7 @@
             // 
             closeWindowButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             closeWindowButton.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            closeWindowButton.Location = new Point(3, 173);
+            closeWindowButton.Location = new Point(3, 438);
             closeWindowButton.Name = "closeWindowButton";
             closeWindowButton.Size = new Size(505, 34);
             closeWindowButton.TabIndex = 1;
@@ -138,17 +166,186 @@
             closeWindowButton.UseVisualStyleBackColor = true;
             closeWindowButton.Click += closeWindowButton_Click;
             // 
+            // incompleteAllocsCheckBox
+            // 
+            incompleteAllocsCheckBox.AutoSize = true;
+            incompleteAllocsCheckBox.Location = new Point(3, 143);
+            incompleteAllocsCheckBox.Name = "incompleteAllocsCheckBox";
+            incompleteAllocsCheckBox.Size = new Size(83, 19);
+            incompleteAllocsCheckBox.TabIndex = 2;
+            incompleteAllocsCheckBox.Text = "checkBox1";
+            incompleteAllocsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel3.ColumnCount = 3;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333359F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333359F));
+            tableLayoutPanel3.Controls.Add(label2, 0, 0);
+            tableLayoutPanel3.Controls.Add(label3, 0, 1);
+            tableLayoutPanel3.Controls.Add(this.allocationFolderTextBox, 1, 1);
+            tableLayoutPanel3.Controls.Add(this.allocationFolderUploadButton, 2, 1);
+            tableLayoutPanel3.Location = new Point(3, 218);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 2;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle());
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Size = new Size(505, 94);
+            tableLayoutPanel3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            tableLayoutPanel3.SetColumnSpan(label2, 3);
+            label2.Location = new Point(3, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(499, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Output Folder Selection";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 15);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 1;
+            label3.Text = "label3";
+            // 
+            // allocationFolderTextBox
+            // 
+            this.allocationFolderTextBox.Location = new Point(171, 18);
+            this.allocationFolderTextBox.Name = "allocationFolderTextBox";
+            this.allocationFolderTextBox.Size = new Size(100, 23);
+            this.allocationFolderTextBox.TabIndex = 3;
+            // 
+            // allocationFolderUploadButton
+            // 
+            this.allocationFolderUploadButton.Location = new Point(339, 18);
+            this.allocationFolderUploadButton.Name = "allocationFolderUploadButton";
+            this.allocationFolderUploadButton.Size = new Size(75, 23);
+            this.allocationFolderUploadButton.TabIndex = 4;
+            this.allocationFolderUploadButton.Text = "button1";
+            this.allocationFolderUploadButton.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel4.ColumnCount = 2;
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.Controls.Add(allocationFileNameTextBox, 1, 0);
+            tableLayoutPanel4.Controls.Add(label5, 0, 0);
+            tableLayoutPanel4.Location = new Point(3, 318);
+            tableLayoutPanel4.Name = "tableLayoutPanel4";
+            tableLayoutPanel4.RowCount = 1;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.Size = new Size(505, 74);
+            tableLayoutPanel4.TabIndex = 6;
+            // 
+            // allocationFileNameTextBox
+            // 
+            allocationFileNameTextBox.Location = new Point(255, 3);
+            allocationFileNameTextBox.Name = "allocationFileNameTextBox";
+            allocationFileNameTextBox.Size = new Size(100, 23);
+            allocationFileNameTextBox.TabIndex = 0;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 2;
+            label5.Text = "label5";
+            // 
+            // tableLayoutPanel5
+            // 
+            tableLayoutPanel5.ColumnCount = 2;
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel5.Controls.Add(label4, 0, 0);
+            tableLayoutPanel5.Controls.Add(advancedSettingsButton, 1, 0);
+            tableLayoutPanel5.Location = new Point(3, 398);
+            tableLayoutPanel5.Name = "tableLayoutPanel5";
+            tableLayoutPanel5.RowCount = 1;
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel5.Size = new Size(200, 34);
+            tableLayoutPanel5.TabIndex = 7;
+            // 
+            // tableLayoutPanel6
+            // 
+            tableLayoutPanel6.ColumnCount = 2;
+            tableLayoutPanel6.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel6.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel6.Controls.Add(bufferSizesButton, 0, 0);
+            tableLayoutPanel6.Controls.Add(busRateButton, 1, 0);
+            tableLayoutPanel6.Location = new Point(3, 168);
+            tableLayoutPanel6.Name = "tableLayoutPanel6";
+            tableLayoutPanel6.RowCount = 1;
+            tableLayoutPanel6.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel6.Size = new Size(200, 44);
+            tableLayoutPanel6.TabIndex = 8;
+            // 
+            // bufferSizesButton
+            // 
+            bufferSizesButton.Location = new Point(3, 3);
+            bufferSizesButton.Name = "bufferSizesButton";
+            bufferSizesButton.Size = new Size(75, 23);
+            bufferSizesButton.TabIndex = 3;
+            bufferSizesButton.Text = "button1";
+            bufferSizesButton.UseVisualStyleBackColor = true;
+            // 
+            // busRateButton
+            // 
+            busRateButton.Location = new Point(103, 3);
+            busRateButton.Name = "busRateButton";
+            busRateButton.Size = new Size(75, 23);
+            busRateButton.TabIndex = 4;
+            busRateButton.Text = "button2";
+            busRateButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 0;
+            label4.Text = "label4";
+            // 
+            // advancedSettingsButton
+            // 
+            advancedSettingsButton.Location = new Point(103, 3);
+            advancedSettingsButton.Name = "advancedSettingsButton";
+            advancedSettingsButton.Size = new Size(75, 23);
+            advancedSettingsButton.TabIndex = 1;
+            advancedSettingsButton.Text = "button2";
+            advancedSettingsButton.UseVisualStyleBackColor = true;
+            // 
             // SettingsForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(511, 450);
+            ClientSize = new Size(511, 471);
             Controls.Add(tableLayoutPanel1);
             Name = "SettingsForm";
             Text = "Settings";
             tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             tableLayoutPanel2.ResumeLayout(false);
             tableLayoutPanel2.PerformLayout();
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel3.PerformLayout();
+            tableLayoutPanel4.ResumeLayout(false);
+            tableLayoutPanel4.PerformLayout();
+            tableLayoutPanel5.ResumeLayout(false);
+            tableLayoutPanel5.PerformLayout();
+            tableLayoutPanel6.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -162,5 +359,24 @@
         private RadioButton totalModeRadioButton;
         private Label modeDescriptionLabel;
         private Button closeWindowButton;
+        private CheckBox incompleteAllocsCheckBox;
+        private Button bufferSizesButton;
+        private Button busRateButton;
+        private TableLayoutPanel tableLayoutPanel3;
+        private TableLayoutPanel tableLayoutPanel4;
+        private TableLayoutPanel tableLayoutPanel5;
+        private TableLayoutPanel tableLayoutPanel6;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private TextBox allocationFileNameTextBox;
+        private TextBox textBox4;
+        private Label label5;
+        private Label label4;
+        private Button advancedSettingsButton;
+        private Label label6;
     }
 }
